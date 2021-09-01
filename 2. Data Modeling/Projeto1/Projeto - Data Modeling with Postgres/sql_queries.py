499d187f9b8d9a73543ec72c5b1ce91e037d8193
Thays de Oliveira Coelho <thays.coelho@epl.gov.br>
2021-09-01 08:59:02 -0300

    Exercícios e projeto 1 do módulo 2. Data Modeling

---
+++ 2. Data Modeling/Projeto1/Projeto - Data Modeling with Postgres/sql_queries.py
@@ -0,0 +1,153 @@
+# DROP TABLES
+
+songplay_table_drop = "DROP table IF EXISTS songplays"
+user_table_drop = "DROP table IF EXISTS users"
+song_table_drop = "DROP table IF EXISTS songs"
+artist_table_drop = "DROP table IF EXISTS artists"
+time_table_drop = "DROP table IF EXISTS time"
+
+# CREATE TABLES
+songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays (
+    songplay_id serial PRIMARY KEY
+    , start_time timestamp REFERENCES time(start_time)
+    , user_id int NOT NULL REFERENCES users(user_id)
+    , level varchar
+    , song_id text REFERENCES songs(song_id)
+    , artist_id text REFERENCES artists(artist_id)
+    , session_id int not null
+    , location varchar
+    , user_agent text);
+""")
+
+user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users (
+    user_id int PRIMARY KEY
+    , first_name text NOT NULL
+    , last_name text NOT NULL
+    , gender varchar
+    , level varchar);
+""")
+
+song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs (
+    song_id varchar PRIMARY KEY
+    , title varchar NOT NULL
+    , artist_id text NOT NULL
+    , year int
+    , duration numeric NOT NULL);
+""")
+
+artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists (
+    artist_id varchar PRIMARY KEY
+    , artist_name varchar NOT NULL
+    , artist_location varchar
+    , artist_latitude varchar
+    , artist_longitude varchar);
+""")
+
+time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time (
+    start_time timestamp PRIMARY KEY
+    , hour int
+    , day int
+    , week int
+    , month int
+    , year int
+    , weekday int);
+""")
+
+# INSERT RECORDS
+songplay_table_insert = ("""
+INSERT INTO songplays(
+    start_time
+    , user_id
+    , level
+    , song_id
+    , artist_id
+    , session_id
+    , location
+    , user_agent)
+values(%s, %s, %s, %s, %s, %s, %s, %s)
+""")
+
+
+user_table_insert = ("""
+INSERT INTO users(
+    user_id
+    , first_name
+    , last_name
+    , gender
+    , level)
+VALUES(%s, %s, %s, %s, %s)
+ON CONFLICT (user_id) DO UPDATE set level = EXCLUDED.level
+""")
+
+song_table_insert = ("""
+INSERT INTO songs(
+    song_id
+    , title
+    , artist_id
+    , year
+    , duration)
+VALUES(%s, %s, %s, %s, %s)
+ON CONFLICT (song_id) DO NOTHING
+""")
+
+artist_table_insert = ("""
+INSERT INTO artists(
+    artist_id
+    , artist_name
+    , artist_location
+    , artist_latitude
+    , artist_longitude)
+VALUES(%s, %s, %s, %s, %s)
+ON CONFLICT (artist_id) DO NOTHING
+""")
+
+
+time_table_insert = ("""
+INSERT INTO time(
+    start_time
+    , hour
+    , day
+    , week
+    , month
+    , year
+    , weekday)
+VALUES(%s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT (start_time) DO NOTHING
+""")
+
+# FIND SONGS
+
+song_select = ("""
+select
+    s.song_id
+    , s.artist_id
+from
+    songs s
+    inner join artists a
+        on a.artist_id=s.artist_id    
+where
+    s.title = %s
+    and a.artist_name = %s
+    and s.duration = %s;
+""")
+
+# QUERY LISTS
+create_table_queries = [
+    user_table_create,
+    artist_table_create,
+    song_table_create,
+    time_table_create,
+    songplay_table_create
+]
+drop_table_queries = [
+    user_table_drop,
+    artist_table_drop,
+    song_table_drop,
+    time_table_drop,
+    songplay_table_drop,
+]
